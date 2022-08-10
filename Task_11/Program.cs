b2112b5fe044a42c76ad96bf75ffae71b6b4c694
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:31:13 +0300

    Task 11 made

---
+++ Task_11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int Number = new Random().Next(1, 2000);
+System.Console.WriteLine($"Дано число: {Number}");
+
+if (Number / 100 > 0) System.Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+else System.Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+System.Console.WriteLine();
