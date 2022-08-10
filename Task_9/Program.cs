e00efe21463cbadf6443f75ecdc0709d5d6e9623
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:30:22 +0300

    Task 9 made

---
+++ Task_9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Вставить трехзначное число, ");
+int x=int.Parse(Console.ReadLine());
+int y =(x/10)-(x%100/10)+x%10;
+
+
+Console.WriteLine(y);
