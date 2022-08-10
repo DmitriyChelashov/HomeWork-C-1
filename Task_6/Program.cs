c1bc16519075233512cad512507441fda691857f
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:29:02 +0300

    Task 6 made

---
+++ Task_6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number=int.Parse(Console.ReadLine());
+
+int x =number %10;
+
+Console.WriteLine("Последняя цифра: " + x);
