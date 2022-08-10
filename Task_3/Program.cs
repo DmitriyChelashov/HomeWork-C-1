75cfddcb6efcd39fde51c8fcf6cd82d109524cdb
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:26:21 +0300

    Task 3 made

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine ("Введите первое число: ");
+int x = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int y = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите третье число: ");
+int z = int.Parse (Console.ReadLine());
+
+int max = x;
+
+if (y > x)
+{
+    max = y;
+}
+if (z > y)
+{
+    max = z;
+}
+if (x > z)
+{
+    max = x;
+}
+
+Console.WriteLine (max);
---
+++ Task_3/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine ("Введите целое число, чтобы проверить его четность: ");
+int x = int.Parse (Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine ("Да, число является четным!");
+}
+else
+{
+    Console.WriteLine ("Не, нифига оно не четное!");
+}
