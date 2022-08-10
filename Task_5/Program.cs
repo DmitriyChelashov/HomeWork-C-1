df21d8aff274e3ff90d45039c7cea84c6ccbee7a
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:28:28 +0300

    Task 5 made

---
+++ Task_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine ("Введите число N для которого мы покажем числа -N до N: ");
+int UserNumber = int.Parse(Console.ReadLine ());
+
+int count = 1;
+
+Console.WriteLine (" ");
+
+while (count <= UserNumber)
+{
+    Console.Write (count + " ");
+    Console.Write ("-" + count + " ");
+    count++;
+}
+
