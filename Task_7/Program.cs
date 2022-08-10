0df21153e33fbdb19256d977c54e107dd4148658
«Dmitriy» <arsenal_lion@mail.ru>
2022-08-10 23:29:26 +0300

    Task 7 made

---
+++ Task_7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number =int.Parse(Console.ReadLine());
+
+int x =(number % 100 -number % 10)/10;
+Console.WriteLine("Вторая цифра: " + x);
